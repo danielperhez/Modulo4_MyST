f942b3eaa1f461fb1a4340d0ae42882f912b7931
Daniel Pérez <carlosdanielph11@gmail.com>
2022-04-28 21:03:31 -0500

    agregar funciones gráficas

--- main.py
+++ main.py
@@ -13,3 +13,43 @@ import visualizations
 def crypto_exchanges(minutes):
     avance_1,avance_2,rolls = functions.cxt(minutes)
     return avance_1,avance_2,rolls
+
+def plot_bid_bit_1(data):
+    a = visualizations.plot_bid_bit(data)
+    return a
+
+def plot_ask_bit_1(data):
+    a = visualizations.plot_ask_bit(data)
+    return a
+
+def plot_spread_bit_1(data):
+    a = visualizations.plot_spread_bit(data)
+    return a
+
+
+
+def plot_bid_eth_1(data):
+    a = visualizations.plot_bid_eth(data)
+    return a
+
+def plot_ask_eth_1(data):
+    a = visualizations.plot_ask_eth(data)
+    return a
+
+def plot_spread_eth_1(data):
+    a = visualizations.plot_spread_eth(data)
+    return a
+
+
+
+def plot_bid_ltc_1(data):
+    a = visualizations.plot_bid_ltc(data)
+    return a
+
+def plot_ask_ltc_1(data):
+    a = visualizations.plot_ask_ltc(data)
+    return a
+
+def plot_spread_ltc_1(data):
+    a = visualizations.plot_spread_ltc(data)
+    return a
--- visualizations.py
+++ visualizations.py
@@ -7,3 +7,227 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
+from symtable import Symbol
+import ccxt
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from sympy import limit
+import time
+import plotly.express as px
+import math
+import plotly.graph_objects as go
+
+def plot_bid_bit(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_bid_bit'],
+        name = 'bitso_bid_bit', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_bid_bit'],
+        name='kraken_bid_bit',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_bid_bit'],
+        name='binance_bid_bit',
+    ))
+
+    return fig.show()
+
+
+def plot_ask_bit(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_bit'],
+        name = 'bitso_ask_bit', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_bit'],
+        name='kraken_ask_bit',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_bit'],
+        name='binance_ask_bit',
+    ))
+
+    return fig.show()
+
+
+def plot_spread_bit(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_bit'],
+        name = 'bitso_ask_bit',
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_bit'],
+        name='kraken_ask_bit',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_bit'],
+        name='binance_ask_bit',
+    ))
+
+    return fig.show()
+
+
+
+
+def plot_bid_eth(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_bid_eth'],
+        name = 'bitso_bid_eth', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_bid_eth'],
+        name='kraken_bid_eth',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_bid_eth'],
+        name='binance_bid_eth',
+    ))
+
+    return fig.show()
+
+
+def plot_ask_eth(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_eth'],
+        name = 'bitso_ask_eth', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_eth'],
+        name='kraken_ask_eth',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_eth'],
+        name='binance_ask_eth',
+    ))
+
+    return fig.show()
+
+
+def plot_spread_eth(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_eth'],
+        name = 'bitso_ask_eth',
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_eth'],
+        name='kraken_ask_eth',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_eth'],
+        name='binance_ask_eth',
+    ))
+
+    return fig.show()
+
+
+def plot_bid_ltc(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_bid_ltc'],
+        name = 'bitso_bid_ltc', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_bid_ltc'],
+        name='kraken_bid_ltc',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_bid_ltc'],
+        name='binance_bid_ltc',
+    ))
+
+    return fig.show()
+
+
+def plot_ask_ltc(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_ltc'],
+        name = 'bitso_ask_ltc', # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_ltc'],
+        name='kraken_ask_ltc',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_ltc'],
+        name='binance_ask_ltc',
+    ))
+
+    return fig.show()
+
+
+def plot_spread_ltc(data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['bitso_ask_ltc'],
+        name = 'bitso_ask_ltc',
+    ))
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['kraken_ask_ltc'],
+        name='kraken_ask_ltc',
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=data['timeframe'],
+        y=data['binance_ask_ltc'],
+        name='binance_ask_ltc',
+    ))
+
+    return fig.show()
